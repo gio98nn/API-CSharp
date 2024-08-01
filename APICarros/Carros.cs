b91f409a5e623b8f82ce092f77f8646b2a4720c0
gio98nn <giovanninuno.contato@gmail.com>
2024-08-01 11:01:42 -0300

    Primeira versão da API base

---
+++ APICarros/Carros.cs
@@ -0,0 +1,11 @@
+﻿namespace APICarros
+{
+    public class Carros
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }    
+        public decimal Preco {  get; set; } 
+    }
+}
---
+++ APICarros/Controllers/CarrosController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace APICarros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarrosController : Controller
+    {
+        [HttpGet]//Verbo http do EndPoint
+        public async Task<ActionResult> Get()//EndPoint
+        {
+            try
+            {
+                var listaCarros = new List<Carros>();
+
+
+                var connectionString = "Host=localhost;Username=postgres;Password=12345;Database=postgres";
+                await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+                // Retrieve all rows
+                await using (var cmd = dataSource.CreateCommand("SELECT * FROM carros"))
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var carro = new Carros
+                        {
+                            Id = reader.GetInt32(0),// Read 1st collum
+                            Marca = reader.GetString(1),
+                            Modelo = reader.GetString(2),
+                            Ano = reader.GetInt32(3),
+                            Preco = reader.GetDecimal(4)
+                        };
+
+                        listaCarros.Add(carro);
+                    }
+                }
+
+
+
+                return Ok(listaCarros);
+            }
+            catch (Exception ex)
+            {
+                //return StatusCode(500); 
+                return BadRequest(ex);
+            }
+        }
+
+
+        [HttpGet("GetOrdered")]//Verbo http do EndPoint
+        public async Task<ActionResult> GetOrdered()// EndPoint
+        {
+            try
+            {
+                var listaCarros = new List<Carros>();
+
+
+                var connectionString = "Host=localhost;Username=postgres;Password=12345;Database=postgres";
+                await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+                // Retrieve all rows
+                await using (var cmd = dataSource.CreateCommand("SELECT * FROM carros ORDER BY preco"))
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var carro = new Carros
+                        {
+                            Id = reader.GetInt32(0),//Read 1st collum
+                            Marca = reader.GetString(1),
+                            Modelo = reader.GetString(2),
+                            Ano = reader.GetInt32(3),
+                            Preco = reader.GetDecimal(4)
+                        };
+
+                        listaCarros.Add(carro);
+                    }
+                }
+
+
+
+                return Ok(listaCarros);
+            }
+            catch (Exception ex)
+            {
+                //return StatusCode(500); 
+                return BadRequest(ex);
+            }
+        }
+    }
+}
